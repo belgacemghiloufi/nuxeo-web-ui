3ee7ba63edaad4969f7b5b69a5bc8c2a470359d0
Miguel Nixo <mnixo@nuxeo.com>
2021-01-02 16:21:28 +0000

    replace nuxeo-dc-title-input and nuxeo-dc-title-label with nuxeo-dc-title

--- elements/compound/nuxeo-dc-title.js
+++ elements/compound/nuxeo-dc-title.js
@@ -8,18 +8,29 @@ import '@nuxeo/nuxeo-ui-elements/widgets/nuxeo-input.js';
 Polymer({
   _template: html`
     <style include="nuxeo-styles"></style>
-    <nuxeo-input
-      label="[[i18n('label.ui.schema.dc.title')]]"
-      name="title"
-      value="{{document.properties.dc:title}}"
-      required$="[[required]]"
-    >
+    <template is="dom-if" if="[[editable]]">
+      <nuxeo-input
+        label="[[i18n('label.ui.schema.dc.title')]]"
+        name="title"
+        value="{{document.properties.dc:title}}"
+        required$="[[required]]"
+      >
+    </template>
+    <template is="dom-if" if="[[!editable]]">
+      <label>[[i18n('label.ui.schema.dc.title')]]</label>
+      <div name="title">[[document.properties.dc:title]]</div>
+    </template>
     </nuxeo-input>
   `,
-  is: 'nuxeo-dc-title-input',
+  is: 'nuxeo-dc-title',
   behaviors: [I18nBehavior],
   properties: {
     document: Object,
+    editable: {
+      type: Boolean,
+      value: false,
+      reflectToAttribute: true,
+    },
     required: {
       type: Boolean,
       value: false,
